4cd7073d963efcd1aa188a31bfc4441f42138ee1
Angel Rodriguez <anxolin@gmail.com>
2018-10-07 23:32:58 +0200

    Make links relative

--- src/util/web3.js
+++ src/util/web3.js
@@ -1,5 +1,12 @@
 import Web3 from 'web3'
-const web3 = new Web3(window.web3.currentProvider)
+
+let web3
+if (typeof window.web3 !== 'undefined') {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // Set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
+}
 
 // Just convinient for quick tests
 window.app = {
